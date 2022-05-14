e8110a0fd76cd0c20b11baf0e7dbc20713d516aa
hundun <wwj.hddjj@gmail.com>
2022-05-14 10:22:14 +0800

    use bitsy-HarvestMashroom project's gamedata, workable

--- input/hacks.js
+++ input/hacks.js
@@ -1,2 +1,4 @@
+import '@bitsy/hecks/src/dialog choices';
+import '@bitsy/hecks/src/dialog jump';
+import '@bitsy/hecks/src/edit image from dialog';
 import '@bitsy/hecks/src/exit-from-dialog';
-import '@bitsy/hecks/src/gamepad input';
