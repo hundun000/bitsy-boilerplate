6019a3c40394941fe782bfe9fdb75a164aba3e3a
hundun <wwj.hddjj@gmail.com>
2022-12-04 11:24:27 +0800

    add music; move hacks folder

--- input/hacks.js
+++ input/hacks.js
@@ -4,5 +4,6 @@ import '../modifiedHacks/edit image from dialog';
 import '@bitsy/hecks/src/exit-from-dialog';
 import '@bitsy/hecks/src/logic-operators-extended';
 import '../modifiedHacks/edit room from dialog';
-import '../modifiedHacks/winter-farm-helper';
-import './custom-keyhandlers';
+import '../projectHacks/winter-farm-helper';
+import '../projectHacks/custom-keyhandlers';
+import '../projectHacks/bitsymuse';
---
+++ projects/winter-farm/asserts/license.md
@@ -0,0 +1,7 @@
+- midi-town-theme-melody 
+Author: CleytonKauffman 
+https://opengameart.org/content/midi-town-theme-melody 
+
+- snow_city
+Author: CleytonKauffman 
+https://opengameart.org/content/midi-town-theme-melody
