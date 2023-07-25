702e216d579cb09b5fd6617467c87b7895447392
hundun <wwj.hddjj@gmail.com>
2023-07-25 10:35:11 +0800

    tower back to v7 work

--- input/hacks.js
+++ input/hacks.js
@@ -1,10 +1,10 @@
 import '@bitsy/hecks/src/dialog choices';
 import '@bitsy/hecks/src/long dialog';
 import '@bitsy/hecks/src/dialog jump';
-import '../modifiedHacks/edit image from dialog';
-import '../modifiedHacks/edit room from dialog';
+import '../modifiedHacks/v7/edit image from dialog';
+import '../modifiedHacks/v7/edit room from dialog';
 import '@bitsy/hecks/src/javascript dialog';
 import '@bitsy/hecks/src/logic-operators-extended';
-import { hackOptions as saveHackOptions } from '../modifiedHacks/save';
+import { hackOptions as saveHackOptions } from '../modifiedHacks/v7/base-save';
 
 saveHackOptions.loadOnStart = false;
